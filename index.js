90778969f86064cbbaba3b92df77428b25fca704
Quince Lewis <106302729+quinceosanya@users.noreply.github.com>
2022-06-28 14:41:00 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// const divide= (function(number1,number2){
+// const division = number1 / number2;
+//     return division;
+// })
+
+// divide(2000,100);
+
+//Arrow function 
+// let divide = function divideExpression(number1,number2){
+//     let divison = number1/number2;
+//     return divison;
+// }
+
+// console.log(divide(2000,100));
+
+const divide = function(){
+    return 2000/100;
+}
+
+const square = number => number * number;
+
+const add = (paramOne,paramTwo) => paramOne + paramTwo;
